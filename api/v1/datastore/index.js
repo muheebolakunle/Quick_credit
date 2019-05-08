8160048f817e2ad465af342d1a98b59f565fadd9
Muheeb Olakunle <olakunlemuheeb@gmail.com>
2019-05-08 03:54:37 +0100

    feat(api endpoint): implement signin endpoint
    -implement endpoint that allows a user to signin
    -write test to cover endpoint functionality
    
    [Finishes #165813990]

--- api/v1/datastore/index.js
+++ api/v1/datastore/index.js
@@ -1,3 +1,32 @@
-export const userStore = [];
+export const userStore = [
+  {
+    id: 1,
+    email: 'olakunle@gmail.com',
+    firstName: 'olakunle',
+    lastName: 'muheeb',
+    password: '$2b$10$M54YQLCOj7/OOi9TrdHxhO7rXk5zHTypjcn0X/sg8K1RANNi60LtW',
+    address: 'festac town',
+    status: 'verified',
+    isAdmin: true
+  }, {
+    id: 2,
+    email: 'olaide@gmail.com',
+    firstName: 'olaide',
+    lastName: 'muheeb',
+    password: '$2b$10$PjVGFLHaft8.FMHrjGxhqe.60WPaim3LOFxHjAwi6qaUY6XOGGeqK',
+    address: 'festac town',
+    status: 'unverified',
+    isAdmin: false
+  }, {
+    id: 3,
+    email: 'olabisi@gmail.com',
+    firstName: 'olabisi',
+    lastName: 'muheeb',
+    password: '$2b$10$KToRlR9TU/Ll44TuLqeRh.KfY5p82//YyG/v8kWwUmr.p8UfWK97G',
+    address: 'ajibode opp ui gate',
+    status: 'verified',
+    isAdmin: false
+  }
+];
 export const loanStore = [];
 export const repaymentstore = [];
