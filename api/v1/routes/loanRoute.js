ce8ea7881d3c4ca8a21c4fdc4259bda560571895
Muheeb Olakunle <olakunlemuheeb@gmail.com>
2019-05-10 09:49:57 +0100

    feat(loan): implement view specific loan
    -write test and ensure they are failing
    -write code to implement this feature
    
    [Finishes #165930610]

--- api/v1/routes/loanRoute.js
+++ api/v1/routes/loanRoute.js
@@ -4,12 +4,13 @@ import LoanValidation from '../middleware/loanvalidation';
 import { auth, adminAuth } from '../middleware/authentication';
 
 const { validateLoan, validateQuery } = LoanValidation;
-const { createLoan, getAllLoans } = loanController;
+const { createLoan, getAllLoans, getLoanById } = loanController;
 
 const router = express.Router();
 
 router.post('/', auth, validateLoan, createLoan);
 router.get('/', auth, adminAuth, validateQuery, getAllLoans);
+router.get('/:id', auth, adminAuth, getLoanById);
 
 
 export default router;
