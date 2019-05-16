c35c9222c07a8f66d608f3cf18fa802e7aab10dd
Muheeb Olakunle <olakunlemuheeb@gmail.com>
2019-05-16 01:01:18 +0100

    feat(UI): create page to verify user
    [Finishes #166038728]

--- UI/app.js
+++ UI/app.js
@@ -1,20 +1,34 @@
-document.getElementById("apply").addEventListener("click", function(e) {
-    e.preventDefault();
-  });
+const modal = document.querySelector('.modal');
+const trigger = document.querySelector('.trigger');
+const closeButton = document.querySelector('.close-button');
+const closeButton2 = document.querySelector('.close-button2');
+const verified = document.querySelector('#verify');
 
+function toggleModal() {
+  modal.classList.toggle('show-modal');
+}
+
+function verify() {
+  document.getElementById('user-status').innerHTML = 'Status: ' + 'VERIFIED';
+}
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+closeButton2.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnClick);
+verified.addEventListener('click', verify);
+
+document.getElementById('apply').addEventListener('click', (e) => {
+  e.preventDefault();
+});
 
 
 const show = (id) => {
-    let e = document.getElementById(id);
-    e.style.display = e.style.display == "none" ? "block" : "block";
-  };
-
-  function verify() {
-    if(document.getElementById('verify').checked === true){
-      document.getElementById('user-status').innerHTML = 'Status: '+ 'VERIFIED'; 
-    }else{
-      document.getElementById('user-status').innerHTML = 'Status: '+ 'Unverified'; 
-    }
-  }
-  
-  
+  const e = document.getElementById(id);
+  e.style.display = e.style.display == 'none' ? 'block' : 'block';
+};
