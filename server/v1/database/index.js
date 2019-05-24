8b16d90d131bb49a85a6496c55daa052f5614294
Muheeb Olakunle <olakunlemuheeb@gmail.com>
2019-05-24 04:51:13 +0100

    update

--- server/v1/database/index.js
+++ server/v1/database/index.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'test') {
   connectionString = process.env.DATABASE_URL_TEST;
 }
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   connectionString = process.env.DATABASE_URL;
 }
 
