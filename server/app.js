1d75d21cf85399aec58b30f46ae1d73afcb038ef
Muheeb Olakunle <olakunlemuheeb@gmail.com>
2020-01-27 15:33:54 +0100

    chore: create api documetation [Finished #170147188]

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './v1/routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1', routes);
